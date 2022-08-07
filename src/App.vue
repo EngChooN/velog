c6600067ada20b91db5de1589027096dcf8baf47
준영 <aimzero9303@gmail.com>
2022-08-08 00:37:56 +0900

    listComponent&bootstrapNavbar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
-
   <!-- 부트스트랩 연습 -->
-  <button type="button" class="btn btn-primary">Bootstrap Button</button>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">VueLogProject</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- 글 리스트 컴포넌트 (더미데이터) -->
+  <div class="Explanation">
+    <h5>React 유저의 Vue3로 블로그 만들기 프로젝트..</h5>
+    <p>- Products by aimZero -</p>
+  </div>
+
+  <ListComponent :blogData="blogData" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ListComponent from "./components/List.vue";
+import blogData from "./assets/data/blog";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    ListComponent: ListComponent,
+  },
+  data() {
+    return {
+      blogData: blogData,
+    };
   },
 };
 </script>
@@ -22,8 +64,15 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.Explanation {
+  text-align: center;
+  margin-bottom: 50px;
+  padding-top: 30px;
+  padding-bottom: 10px;
+  background-color: rgb(223, 223, 223);
 }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="listWrapper">
+    <div v-for="(el, index) in blogData" :key="index" class="list">
+      <h4>{{ el.title }}</h4>
+      <p>{{ el.date }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListComponent",
+  props: {
+    blogData: Array,
+  },
+};
+</script>
+
+<style>
+.listWrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.list {
+  max-width: 500px;
+  width: 100%;
+  margin-bottom: 50px;
+  border: 1px solid rgb(173, 173, 173);
+  padding: 30px;
+}
+</style>
